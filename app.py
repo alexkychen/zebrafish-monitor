3d02a4e31301826fbc5e9667b08c90830bf1e0f5
alexkychen <alexkychen@gmail.com>
2023-02-05 19:07:13 -0500

    update

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import pandas as pd
 import numpy as np
 from datetime import timedelta
 import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 
 from statsmodels.tsa.seasonal import seasonal_decompose
 from statsmodels.tsa.stattools import adfuller
@@ -113,9 +115,20 @@ def ets_decomposition(df, choices, freq):
     #co1.metric(label="Period",value=calculated_period, help="Period = 1440 / freq (min)")
     try:
         results_add = seasonal_decompose(df[selected_data], model=selected_model, period=calculated_period)
-        co2.write(results_add)
-        co2.plotly_chart(results_add.plot())
-        #co2.plotly_chart(results_add.plot(),  use_container_width=True)
+        #create ETS decomposition plot
+        fig = make_subplots(rows=4, cols=1)
+        fig.add_trace(go.Scatter(x=results_add.observed.index, y=results_add.observed), row=1, col=1)
+        fig.add_trace(go.Scatter(x=results_add.trend.index, y=results_add.trend), row=2, col=1)
+        fig.add_trace(go.Scatter(x=results_add.seasonal.index, y=results_add.seasonal), row=3, col=1)
+        fig.add_trace(go.Scatter(x=results_add.resid.index, y=results_add.resid, mode='markers'), row=4, col=1)
+        fig.add_hline(y=0, row=4, col=1)
+        fig.update_yaxes(title_text="Observed", row=1, col=1)
+        fig.update_yaxes(title_text="Trend", row=2, col=1)
+        fig.update_yaxes(title_text="Seasonal", row=3, col=1)
+        fig.update_yaxes(title_text="Residual", row=4, col=1)
+        fig.update_layout(height=600, showlegend=False)
+        co2.plotly_chart(fig, use_container_width=True)
+        #st.plotly_chart(results_add.plot())
     except:
         co2.error("Error: Can't generate plots. Please select data or adjust parameters (e.g., Resample frequency).")
     return selected_data
